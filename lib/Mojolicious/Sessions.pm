a252a273f7899547f83ef7867297e23309f7614d
Achmad Yusri Afandi <linuxer08@gmail.com>
2018-08-13 14:14:45 +0800

    Fix updating max-age support into Mojolicious::Sessions

--- lib/Mojolicious/Sessions.pm
+++ lib/Mojolicious/Sessions.pm
@@ -4,7 +4,7 @@ use Mojo::Base -base;
 use Mojo::JSON;
 use Mojo::Util qw(b64_decode b64_encode);
 
-has [qw(cookie_domain secure)];
+has [qw(cookie_domain secure max_age)];
 has cookie_name        => 'mojolicious';
 has cookie_path        => '/';
 has default_expiration => 3600;
@@ -31,30 +31,34 @@ sub load {
 
 sub store {
   my ($self, $c) = @_;
-
+  
   # Make sure session was active
   my $stash = $c->stash;
   return unless my $session = $stash->{'mojo.session'};
   return unless keys %$session || $stash->{'mojo.active_session'};
-
+  
   # Don't reset flash for static files
   my $old = delete $session->{flash};
   $session->{new_flash} = $old if $stash->{'mojo.static'};
   delete $session->{new_flash} unless keys %{$session->{new_flash}};
-
+  
   # Generate "expires" value from "expiration" if necessary
   my $expiration = $session->{expiration} // $self->default_expiration;
   my $default    = delete $session->{expires};
   $session->{expires} = $default || time + $expiration
     if $expiration || $default;
-
+  
+  # The "max_age" attribute is specified if needed
+  my $max_age = $session->{expires} - time
+    if $self->max_age && $session->{expires} > time;
+  
   my $value = b64_encode $self->serialize->($session), '';
   $value =~ y/=/-/;
   my $options = {
     domain   => $self->cookie_domain,
     expires  => $session->{expires},
     httponly => 1,
-    max_age  => $session->{expires} > 1 ? $session->{expires} - time : undef,
+    max_age  => $max_age,
     path     => $self->cookie_path,
     secure   => $self->secure
   };
@@ -140,6 +144,16 @@ A callback used to deserialize sessions, defaults to L<Mojo::JSON/"j">.
     my $bytes = shift;
     return {};
   });
+
+=head2 max_age
+
+  $sessions->max_age($bool);
+  
+Set the C<max-age> on all session cookies. If "max_age" is set, session cookie
+will expires based on the number of seconds from attribute "default_expiration".
+Before set this attribute, please see
+L<Browser compatibility.|
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie#Browser_compatibility>
 
 =head2 secure
 
